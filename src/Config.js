96f30407191b1d60d3337b656d45c9e8d46d4eac
Ammar <ammar@shoonya.io>
2019-09-10 23:35:36 +0530

    adding project config : port, infura address, contract address, startblock number

---
+++ src/Config.js
@@ -0,0 +1,8 @@
+const Config = {
+    "PORT": 8080,
+    "NETWORK": "https://mainnet.infura.io/v3/18f52ed927a64f57ad1810599a18e209",
+    "CONTRACT": "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb",
+    "STARTBLOCK": 3914495
+}
+
+module.exports = Config
